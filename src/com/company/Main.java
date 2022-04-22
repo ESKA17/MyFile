f07c44cf51df1b4c3eaaaa4f8b703f660b72ccc5
ESKA17 <sabakurmashev@gmail.com>
2022-04-22 15:08:36 +0600

    Second draft

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,22 @@
 package com.company;
 
 import static com.company.MyFile.listDirectory;
+import static com.company.MyFile.listPythonFiles;
 
-import java.util.Objects;
 import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String input = null;
-        while (!Objects.equals(input, "exit")) {
+        String input;
+        while (true){
             Scanner scan = new Scanner(System.in);
             input = scan.nextLine();
             if (input.startsWith("ls ")) {
                 listDirectory(input);
+            } else if (input.equals("exit")) {
+                return;
+            } else if (input.startsWith("ls_py ")) {
+                listPythonFiles(input);
             } else {
                 System.out.println(input + ": not found");
             }
--- src/com/company/MyFile.java
+++ src/com/company/MyFile.java
@@ -5,12 +5,12 @@ import java.io.File;
 class MyFile {
     // выводит список всех файлов и директорий для `path` - ls
     public static void listDirectory(String path) {
-        path = path.substring(3);
-        if (path.isBlank()) {path = "./";}
-        File folder = new File(path);
-        File[] listOfFiles = folder.listFiles();
+        File[] listOfFiles = listMeFiles(path, 3);
         try {
-            assert listOfFiles != null;
+            if (listOfFiles == null) {
+                System.out.println("No files or folders in the directory!");
+                return;
+            }
             for (File fileCheck : listOfFiles) {
                 if (fileCheck.isFile() || fileCheck.isDirectory()) {
                     System.out.println(fileCheck.getName());
@@ -19,5 +19,32 @@ class MyFile {
         } catch (Exception e) {
             System.out.println("ls: cannot access '" + path + "': No such file or directory");
         }
+    }
+
+    // выводит список файлов с расширением `.py` в `path` - ls_py
+    public static void listPythonFiles(String path) {
+        File[] listOfFiles = listMeFiles(path, 6);
+        try {
+            if (listOfFiles == null) {
+                System.out.println("No .py files in the directory!");
+                return;
+            }
+            for (File fileCheck : listOfFiles) {
+                if (fileCheck.isFile() && fileCheck.getName().contains(".py")) {
+                    System.out.println(fileCheck.getName());
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("ls_py: cannot access '" + path + "': No such file or directory");
+        }
+    }
+    // выводит `true`, если `path` это директория, в других случаях `false` - is_dir
+
+
+    private static File[] listMeFiles(String path, int subStr) {
+        path = path.substring(subStr);
+        if (path.isBlank()) {path = "./";}
+        File folder = new File(path);
+        return folder.listFiles();
     }
 }
